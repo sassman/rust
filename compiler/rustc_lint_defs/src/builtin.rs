1e0805b8c140ce5a14729ebe4210e0abb72bfe98
Sven Kanoldt <sven@d34dl0ck.me>
2024-10-15 11:41:20 +0200

    Apply suggestions from code review
    Co-authored-by: Urgau <3616612+Urgau@users.noreply.github.com>

--- compiler/rustc_lint_defs/src/builtin.rs
+++ compiler/rustc_lint_defs/src/builtin.rs
@@ -5034,8 +5034,9 @@ declare_lint! {
 }
 
 declare_lint! {
-    /// The `mixed_export_name_and_no_mangle` lint detects mixed usage of `export_name` and `no_mangle`
-    /// where `no_mangle` is not used by the compiler.
+    /// The `mixed_export_name_and_no_mangle` lint detects usage of both
+    /// `#[export_name]` and `#[no_mangle]` on the same item which result
+    /// on `#[no_mangle]` being ignored.
     ///
     /// ### Example
     ///
@@ -5058,5 +5059,5 @@ declare_lint! {
     ///
     pub MIXED_EXPORT_NAME_AND_NO_MANGLE,
     Warn,
-    "mixed usage of export_name and no_mangle, where no_mangle is not used by the compiler"
+    "mixed usage of `#[export_name]` and `#[no_mangle]`, where `#[export_name]` takes precedence"
 }
